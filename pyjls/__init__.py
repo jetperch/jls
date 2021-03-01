226fdb28e08d69479138177d2078ba913e6445f0
Matt Liberty <matt.liberty@jetperch.com>
2021-03-01 16:32:52 -0500

    Adjusted imports to make pyinstaller happy.

--- pyjls/__init__.py
+++ pyjls/__init__.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .binding import DataType, AnnotationType, SignalType, Writer, Reader, SourceDef, SignalDef, SummaryFSR
+from .binding import DataType, AnnotationType, SignalType, Writer, Reader, SummaryFSR
+from .structs import SourceDef, SignalDef
 from .version import *
 
 __all__ = [Writer, Reader, __version__, DataType, AnnotationType, SignalType, SourceDef, SignalDef, SummaryFSR]
